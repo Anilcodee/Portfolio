b942f5dbca5a59554717721b446d91e264233809
Anilcodee <anil24076@iiitd.ac.in>
2026-01-03 12:09:25 +0530

    Change cookie SameSite attribute to 'none'

--- BackEnd/controllers/authController.js
+++ BackEnd/controllers/authController.js
@@ -78,7 +78,7 @@ export const loginAdmin = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV == 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         })
 
